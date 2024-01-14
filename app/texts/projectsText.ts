658b3ef6a42b669263e3e7164da985e036b11466
SirPripyat <08.leo.rossi@gmail.com>
2024-01-14 09:43:58 -0300

    Add pokedex API na seção de projetos

--- app/texts/projectsText.ts
+++ app/texts/projectsText.ts
@@ -16,9 +16,17 @@ export const projectText: ProjectsSectionText = {
       {
         title: "Pokedéx",
         description:
-          "Pokédex utilizando ReactJs, NextJs 13, Typescript e TailwindCSS",
+          "Pokédex desenvolvida utilizando ReactJs, NextJs 13, TypeScript e TailwindCSS",
         link: "https://pokedex-next-ca26ec4b27cc.herokuapp.com/",
         githubLink: "https://github.com/SirPripyat/pokemon",
+        image: pokemonLogo,
+      },
+      {
+        title: "Pokedéx API",
+        description:
+          "Pokédex desenvolvida utilizando TypeScript, NodeJs, Express e MongoDB, com base na PokéAPI. Atualmente, está alimentando o meu projeto Pokedéx.",
+        link: "https://pokemon-api-d4e8682433d8.herokuapp.com/pokemon?page=1",
+        githubLink: "https://github.com/SirPripyat/pokemon-node-api",
         image: pokemonLogo,
       },
     ],
@@ -29,9 +37,17 @@ export const projectText: ProjectsSectionText = {
       {
         title: "Pokedéx",
         description:
-          "Pokédex using ReactJs, NextJs 13, Typescript and TailwindCSS",
+          "Pokédex developed using ReactJs, NextJs 13, TypeScript and TailwindCSS",
         link: "https://pokedex-next-ca26ec4b27cc.herokuapp.com/",
         githubLink: "https://github.com/SirPripyat/pokemon",
+        image: pokemonLogo,
+      },
+      {
+        title: "Pokedéx API",
+        description:
+          "Pokédex developed using TypeScript, NodeJs, Express and MongoDB, based on PokéAPI. Actually, this project provides data to my Pokedéx project.",
+        link: "https://pokemon-api-d4e8682433d8.herokuapp.com/pokemon?page=1",
+        githubLink: "https://github.com/SirPripyat/pokemon-node-api",
         image: pokemonLogo,
       },
     ],
